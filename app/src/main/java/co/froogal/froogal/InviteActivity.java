f0a37e5c6605dd3c1288586e525a05db64a96653
rohitsakala <rohitsakala>
2015-07-16 19:22:56 +0530

    Invite Friends done. UI and and photoshop pending due to dependencies

--- app/src/main/java/co/froogal/froogal/InviteActivity.java
+++ app/src/main/java/co/froogal/froogal/InviteActivity.java
@@ -1,5 +1,7 @@
 package co.froogal.froogal;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,19 +9,41 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.util.Log;
+import android.view.Display;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 import java.io.IOException;
 import java.net.URL;
+
+import com.google.android.gms.plus.PlusShare;
 import com.google.android.gms.plus.model.people.Person;
 import java.io.IOException;
 import java.net.URL;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareButton;
+import io.fabric.sdk.android.Fabric;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
 /**
  * Created by akhil on 8/7/15.
  */
 public class InviteActivity extends ActionBarActivity {
+
+    //Animation variables
+    ObjectAnimator bottom_animator;
+    Display display;
+    int width;
+    int height;
+    LinearLayout bottom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +55,9 @@ public class InviteActivity extends ActionBarActivity {
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         getSupportActionBar().setTitle(s);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TwitterAuthConfig authConfig =  new TwitterAuthConfig("pbwuKrP21OYnntNGa8KCFU2zj", "pejM8Ah6KacttWm4JsZU104pXuC34kWUHbkr158Di5cD5Q9kYS");
+        Fabric.with(this, new TwitterCore(authConfig), new TweetComposer());
 
         // Content for sharing
         ShareLinkContent linkContent = new ShareLinkContent.Builder()
@@ -44,7 +71,43 @@ public class InviteActivity extends ActionBarActivity {
         ShareButton shareButton = (ShareButton) findViewById(R.id.fb_share);
         shareButton.setShareContent(linkContent);
 
+        Button share_button = (Button) findViewById(R.id.share_button_t);
+        share_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TweetComposer.Builder builder = new TweetComposer.Builder(InviteActivity.this)
+                        .text("Froogal gives you rewards !.");
+                builder.show();
+            }
+        });
 
+        // google+ share button
+        Button share_g = (Button) findViewById(R.id.share_button);
+        share_g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch the Google+ share dialog with attribution to your app.
+                Intent shareIntent = new PlusShare.Builder(InviteActivity.this)
+                        .setType("text/plain")
+                        .setText("Froogal gives you rewards !")
+                        .setContentUrl(Uri.parse("https://froogal.in"))
+                        .getIntent();
+
+                startActivityForResult(shareIntent, 0);
+            }
+        });
+
+        //TODO UI after jeevan photoshop
+
+
+        // Getting height and width
+        display = getWindowManager().getDefaultDisplay();
+        width = display.getWidth();
+        height = display.getHeight();
+        bottom = (LinearLayout) findViewById(R.id.down_invite);
+
+        startAnimation_bottom().start();
+        // TODO Akhil Singh Referral code to text view
     }
 
 
@@ -57,6 +120,17 @@ public class InviteActivity extends ActionBarActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public ObjectAnimator startAnimation_bottom() {
+
+        bottom_animator = ObjectAnimator.ofFloat(bottom, "translationY", 2000, 0);
+
+        // Set the duration and interpolator for this animation
+        bottom_animator.setDuration(1000);
+        bottom_animator.setInterpolator(new AnimationUtils().loadInterpolator(this, android.R.interpolator.accelerate_quad));
+
+        return bottom_animator;
     }
 
 }
--- app/src/main/java/co/froogal/froogal/MainApplication.java
+++ app/src/main/java/co/froogal/froogal/MainApplication.java
@@ -4,12 +4,22 @@ import android.app.Application;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends Application {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "JkJusWRKDiSjqTAC24SIh92kz";
+    private static final String TWITTER_SECRET = "R94aX3RjdDfCNert8l4OLo0fgE8V7LLmBXdIQTUNr9dE4pVq5C";
+
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+		Fabric.with(this, new Twitter(authConfig));
 		ImageLoader.getInstance()
 				.init(ImageLoaderConfiguration.createDefault(this));
 	}
